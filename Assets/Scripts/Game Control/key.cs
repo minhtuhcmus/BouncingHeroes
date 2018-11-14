c3c59a5ffc0387b6bd95cb30ee8210fa2a398f11
mokongchung <43271366+mokongchung@users.noreply.github.com>
2018-11-14 16:08:58 +0700

    fix bug key

--- Assets/Scripts/Game Control/key.cs
+++ Assets/Scripts/Game Control/key.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class key : MonoBehaviour {
+	
+	private bool check = true;
 
-	void OnTriggerEnter2D(Collider2D  other){
+	void OnTriggerExit2D(Collider2D  other){
 		//Destroy(this.gameObject);
 		//Debug.Log("KEYyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
-		if(other.gameObject.CompareTag("MC")){
+		if(other.gameObject.CompareTag("MC") && check){
+			check = false;
 			Destroy(this.gameObject);
 			//MC_control.instance.hasKey = true;
 			door.instance.hitKey();
