2c44dda8b8bda1af5529217a1711674806fdc3cb
mokongchung <43271366+mokongchung@users.noreply.github.com>
2018-12-08 18:15:12 +0700

    add random position of. check key spaw (#36)

---
+++ Assets/Scripts/Game Control/checkKeyRandom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkKeyRandom : MonoBehaviour {
+
+	public static checkKeyRandom instance;
+	public BoxCollider2D[] allCollider;
+		
+	void Start () {
+		allCollider = GetComponentsInChildren<BoxCollider2D>();
+		//Debug.Log("test");
+		//checkKey( new Vector3( -2.7f,0.5f,0f));
+	}
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+	}
+	
+	public bool checkKey(Vector3 checkPoint){
+		//Debug.Log("1 test" + allCollider.Length);
+		foreach (BoxCollider2D e in allCollider){
+			//Debug.Log("2 test");
+			if (e.bounds.Contains( checkPoint))
+			{
+				Debug.Log("key true +++++++++");
+				//TransformPoint(Vector3.zero));
+				return false;
+			}
+		}
+		Debug.Log(" key faile--------");
+		return true;
+	}
+}
